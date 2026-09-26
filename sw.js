e2e591ca3be1026324a101fa2dabb7edd7c975b8
Ahmad Ghassan Alzoubi <63409089+a-alzoubi-07-11@users.noreply.github.com>
2026-09-26 10:28:29 +0200

    Refresh Phase 2 action fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mbo-netherlands-v6";
+const CACHE="mbo-netherlands-v7";
 const CORE=["./","./index.html","./articles.html","./about.html","./privacy.html","./contact.html","./manifest.webmanifest","./assets/phase1.css","./assets/phase1.js","./assets/phase2.css","./assets/phase2.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
